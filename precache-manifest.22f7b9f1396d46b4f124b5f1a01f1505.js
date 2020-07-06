698feed07adf30040f35efa1029eaade7e758b43
Witold Andreasik <witold.andreasik@gmail.com>
2020-07-06 23:16:49 +0200

    Updates

---
+++ precache-manifest.22f7b9f1396d46b4f124b5f1a01f1505.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "787c51642bb545af98e78f14e06a5bf8",
+    "url": "/weather-app-hooks/index.html"
+  },
+  {
+    "revision": "a0d8c5612b9a3cb02dce",
+    "url": "/weather-app-hooks/static/css/main.56fa20c9.chunk.css"
+  },
+  {
+    "revision": "7cf2182656a62ed6117a",
+    "url": "/weather-app-hooks/static/js/2.d437b892.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app-hooks/static/js/2.d437b892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0d8c5612b9a3cb02dce",
+    "url": "/weather-app-hooks/static/js/main.2e2b2f6e.chunk.js"
+  },
+  {
+    "revision": "533d12f8928525f9d264",
+    "url": "/weather-app-hooks/static/js/runtime-main.3da814a3.js"
+  }
+]);
