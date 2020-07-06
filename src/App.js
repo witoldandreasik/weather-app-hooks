65c18447d670531a5ab9ccef01b5d899ef07f297
Witold Andreasik <witold.andreasik@gmail.com>
2020-07-06 19:59:37 +0200

    add structure and styles, remove unused files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+const api = {
+  key: "7fd90313ba0928133acb3597d89e8ac7",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
+  let currentDate = new Date().toLocaleDateString();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app warm">
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="Search..." />
+        </div>
+        <div className="location-box">
+          <div className="location">New York City, US</div>
+          <div className="date">{currentDate}</div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">15°C</div>
+          <div className="weather">Sunny</div>
+        </div>
+      </main>
     </div>
   );
 }
